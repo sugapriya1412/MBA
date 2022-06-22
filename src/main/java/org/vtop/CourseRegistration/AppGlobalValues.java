bd0acefb013ba2e1cea3db620db8e1defd1b3ffb
Dinesh Venkatesan <dinesh.v@vit.ac.in>
2022-06-22 15:45:46 +0530

    Clear CodeBase

--- src/main/java/org/vtop/CourseRegistration/AppGlobalValues.java
+++
@@ -1,9 +0,0 @@
-package org.vtop.CourseRegistration;
-
-import java.text.SimpleDateFormat;
-
-public class AppGlobalValues 
-{	
-	public static final SimpleDateFormat appDateTimeFormat= new SimpleDateFormat("dd-MMM-yyyy HH:mm");
-	public static final Integer ServerCapacityFixed = 2000;
-}
