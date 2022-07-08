e08a53eebce31802207d5c9c6c330a48cda86dc5
c.Sivaraman <sivaraman@vit.ac.in>
2022-07-08 19:06:16 +0530

    Academics_Course-Registration-New_Commit_2022-07-08_04

--- src/main/java/org/vtop/CourseRegistration/CustomAuthenticationProvider.java
+++ src/main/java/org/vtop/CourseRegistration/CustomAuthenticationProvider.java
@@ -65,7 +65,7 @@ public class CustomAuthenticationProvider extends DaoAuthenticationProvider
 			//Date & Time Setting
 			SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy");		
 			Date startDate = sdf.parse("08-JUL-2022");
-			Date endDate = sdf.parse("08-JUL-2022");
+			Date endDate = sdf.parse("09-JUL-2022");
 			String startTime = "10:00:00", endTime = "23:59:59", allowStartTime = "10:00:00";
 			
 			int adminAuthenticationStatus = 2;
